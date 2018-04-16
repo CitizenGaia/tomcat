5a35c8b52dfb92c2ac5dcb3aeba568643ec47dd3
CitizenGaia@ubuntuOnStick <michaelbroe@gmail.com>
2018-04-16 17:10:49 +0200

    OK responsive tomcat

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,10 +7,12 @@ REDIRECT_PORT=$(expr $LISTEN_PORT + 443 - 080)
 sed -i "s@<Connector port=\"-1\" protocol=\"HTTP/1.1\" connectionTimeout=\"-1\" redirectPort=\"-1\" />@<Connector port=\"$LISTEN_PORT\" protocol=\"HTTP/1.1\" connectionTimeout=\"20000\" redirectPort=\"$REDIRECT_PORT\" />@" $CATALINA_HOME/conf/server.xml
 
 cat /Manifest/version.properties
+
+echo -e "[$1] docker container" >> $LOG_VOLUME/docker-entrypoint.log
 if [ "$1" = 'run' ]; then
-    echo -e "[$(date -I[seconds])] Booting new docker container" >> $LOG_VOLUME/docker-entrypoint.log
+    echo -e "[$(date -Iseconds)] Booting new docker container" >> $LOG_VOLUME/docker-entrypoint.log
     cat /Manifest/version.properties >> $LOG_VOLUME/docker-entrypoint.log
-    echo -e "\n[$(date -I[seconds])]\n" >> $LOG_VOLUME/docker-entrypoint.log
-    exec catalina.sh run > /dev/null 2>&1 #All the output is also written to normal log file.
+    echo -e "\n[$(date -Iseconds)]\n" >> $LOG_VOLUME/docker-entrypoint.log
+    exec ${TOMCAT_HOME}/bin/catalina.sh run > /dev/null 2>&1 #All the output is also written to normal log file.
 fi
 exec "$@"
